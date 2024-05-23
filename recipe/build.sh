63022c2e496f2a4e9e18cb6ba03711c7bee734e5
Marco Esters <mesters@anaconda.com>
2024-05-23 09:50:13 -0700

    Create shortcuts and apps using menuinst v2

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,15 @@
 
 $PYTHON -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir -vvv
 
+mkdir -p "${PREFIX}/Menu"
+sed "s/__PKG_VERSION__/${PKG_VERSION}/" "${RECIPE_DIR}/menu.json" > "${PREFIX}/Menu/${PKG_NAME}_menu.json"
+if [[ $(uname) == Darwin ]]; then
+    ICON_EXT="icns"
+else
+    ICON_EXT="png"
+fi
+cp "${RECIPE_DIR}/spyder.${ICON_EXT}" "${PREFIX}/Menu/"
+
 rm -rf $PREFIX/man
 rm -f $PREFIX/bin/spyder_win_post_install.py
 rm -rf $SP_DIR/Sphinx-*
