b29ef7e05c851b5e7a8e035eeac6da193179225a
Marco Esters <mesters@anaconda.com>
2024-05-23 12:11:42 -0700

    Fix menu json file name for Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,7 @@ IF NOT EXIST (%MENU_DIR%) mkdir %MENU_DIR%
 
 copy %RECIPE_DIR%\spyder.ico %MENU_DIR%\
 if errorlevel 1 exit 1
-copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\spyder_shortcut.json
+copy %RECIPE_DIR%\menu.json %MENU_DIR%\%PKG_NAME%_menu.json
 if errorlevel 1 exit 1
 
 del %SCRIPTS%\spyder_win_post_install.py
