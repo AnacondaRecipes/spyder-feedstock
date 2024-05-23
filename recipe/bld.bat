63a72a1e13d3e22f5bcfa852e75413763c51137a
Marco Esters <mesters@anaconda.com>
2024-05-23 10:57:16 -0700

    Fix logo location

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,7 @@ if errorlevel 1 exit 1
 set MENU_DIR=%PREFIX%\Menu
 IF NOT EXIST (%MENU_DIR%) mkdir %MENU_DIR%
 
-copy %SRC_DIR%\img_src\spyder.ico %MENU_DIR%\
+copy %RECIPE_DIR%\spyder.ico %MENU_DIR%\
 if errorlevel 1 exit 1
 copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\spyder_shortcut.json
 if errorlevel 1 exit 1
