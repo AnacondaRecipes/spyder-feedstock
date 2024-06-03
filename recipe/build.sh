c8b9da9dca4190c7289c139ef23d8584769de60d
Marco Esters <mesters@anaconda.com>
2024-06-03 12:04:37 -0700

    Use menuinst v1 shortcut for older menuinst versions

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,14 +2,17 @@
 
 $PYTHON -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir -vvv
 
-mkdir -p "${PREFIX}/Menu"
-sed "s/__PKG_VERSION__/${PKG_VERSION}/" "${RECIPE_DIR}/menu.json" > "${PREFIX}/Menu/${PKG_NAME}_menu.json"
+MENU_DIR="${PREFIX}/Menu"
+mkdir -p "${MENU_DIR}"
+sed "s/__PKG_VERSION__/${PKG_VERSION}/" "${RECIPE_DIR}/menu-v2.json" > "${MENU_DIR}/${PKG_NAME}_menu.json"
+cp "${MENU_DIR}/${PKG_NAME}_menu.json" "${MENU_DIR}/${PKG_NAME}_menu-v2.json.bak"
+cp "${RECIPE_DIR}/menu-v1.json" "${MENU_DIR}/${PKG_NAME}_menu-v1.json.bak"
 if [[ $(uname) == Darwin ]]; then
     ICON_EXT="icns"
 else
     ICON_EXT="png"
 fi
-cp "${RECIPE_DIR}/spyder.${ICON_EXT}" "${PREFIX}/Menu/"
+cp "${RECIPE_DIR}/spyder.${ICON_EXT}" "${MENU_DIR}/"
 
 rm -rf $PREFIX/man
 rm -f $PREFIX/bin/spyder_win_post_install.py
---
+++ recipe/post-link.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+
+REM The menuinst v2 json file is not compatible with menuinst versions
+REM older than 2.1.0. Copy the appropriate file as the menu file.
+
+SET LOGFILE=%PREFIX%\.messages.txt
+
+FOR /F %%i IN (
+    '%CONDA_PYTHON_EXE% -c "import menuinst; print(tuple(int(x) for x in menuinst.__version__.split(\".\"))[:3] < (2, 1, 0))"'
+) DO (
+    IF "%%~i"=="True" GOTO :use_menuinst_v1
+)
+COPY /y %PREFIX%\Menu\%PKG_NAME%_menu-v2.json.bak %PREFIX%\Menu\%PKG_NAME%_menu.json
+GOTO :exit
+
+:use_menuinst_v1:
+    COPY /y %PREFIX%\Menu\%PKG_NAME%_menu-v1.json.bak %PREFIX%\Menu\%PKG_NAME%_menu.json
+    ECHO. >> %LOGFILE%
+    ECHO Warning: using menuinst v1 shortcuts. >> %LOGFILE%
+    ECHO menuinst v1 is marked as legacy and is no longer maintained. >> %LOGFILE%
+    ECHO Please update menuinst in the base environment and reinstall %PKG_NAME%. >> %LOGFILE%
+    GOTO :exit
+
+:exit
+    EXIT /B %ERRORLEVEL%
