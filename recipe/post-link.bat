0413ee7333fd94066d12371911e0d0347be37637
Marco Esters <mesters@anaconda.com>
2024-06-20 15:56:48 -0700

    Use exit code

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -7,14 +7,8 @@ SET LOGFILE="%PREFIX%\.messages.txt"
 
 REM Cannot use usual FOR-loop way to assign output of command because
 REM prefixes with spaces in the name cannot be properly escaped.
-REM Use a temporary file as a workaround.
-SET TMPLOG="%PREFIX%\.%PKG_NAME%-post-link.tmp.log"
-"%CONDA_PYTHON_EXE%" -c "import menuinst; print(tuple(int(x) for x in menuinst.__version__.split(\".\"))[:3] < (2, 1, 0))" > %TMPLOG%
-IF ERRORLEVEL 1 EXIT 1
-SET /P OLD_MENUINST=<%TMPLOG%
-DEL %TMPLOG%
-IF "%OLD_MENUINST%" == "True" GOTO :use_menuinst_v1
-
+"%CONDA_PYTHON_EXE%" -c "import menuinst, sys; sys.exit(1 if tuple(int(x) for x in menuinst.__version__.split(\".\"))[:3] < (2, 1, 0) else 0)"
+IF %ERRORLEVEL% == 1 GOTO :use_menuinst_v1
 COPY /y "%PREFIX%\Menu\%PKG_NAME%_menu-v2.json.bak" "%PREFIX%\Menu\%PKG_NAME%_menu.json"
 GOTO :exit
 
@@ -27,4 +21,4 @@ GOTO :exit
 GOTO :exit
 
 :exit
-    EXIT /B %ERRORLEVEL%
+    EXIT /B %ERRORLEVEL%
