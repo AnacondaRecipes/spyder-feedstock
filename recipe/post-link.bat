0cf69644da47d028271d9e8bdaad6c24cbf2a253
Marco Esters <mesters@anaconda.com>
2024-06-20 14:14:38 -0700

    Use workaround for paths with spaces

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -1,25 +1,28 @@
 @ECHO OFF
 
 REM The menuinst v2 json file is not compatible with menuinst versions
-REM older than 2.1.0. Copy the appropriate file as the menu file.
+REM REM older than 2.1.0. Copy the appropriate file as the menu file.
 
-SET LOGFILE=%PREFIX%\.messages.txt
+SET LOGFILE="%PREFIX%\.messages.txt"
 
-FOR /F "delims=" %%i IN (
-    '%CONDA_PYTHON_EXE% -c "import menuinst; print(tuple(int(x) for x in menuinst.__version__.split(\".\"))[:3] < (2, 1, 0))"'
-) DO (
-    IF "%%~i"=="True" GOTO :use_menuinst_v1
-)
-COPY /y %PREFIX%\Menu\%PKG_NAME%_menu-v2.json.bak %PREFIX%\Menu\%PKG_NAME%_menu.json
+REM Cannot use usual FOR-loop way to assign output of command because
+REM prefixes with spaces in the name cannot be properly escaped.
+REM Use a temporary file as a workaround.
+SET TMPLOG="%PREFIX%\.post-link.tmp.log"
+"%CONDA_PYTHON_EXE%" -c "import menuinst; print(tuple(int(x) for x in menuinst.__version__.split(\".\"))[:3] < (2, 1, 0))" > %TMPLOG%
+SET /P OLD_MENUINST=<%TMPLOG%
+DEL %TMPLOG% IF "%OLD_MENUINST%" == "True" GOTO :use_menuinst_v1
+
+COPY /y "%PREFIX%\Menu\%PKG_NAME%_menu-v2.json.bak" "%PREFIX%\Menu\%PKG_NAME%_menu.json"
 GOTO :exit
 
 :use_menuinst_v1:
-    COPY /y %PREFIX%\Menu\%PKG_NAME%_menu-v1.json.bak %PREFIX%\Menu\%PKG_NAME%_menu.json
+    COPY /y "%PREFIX%\Menu\%PKG_NAME%_menu-v1.json.bak" "%PREFIX%\Menu\%PKG_NAME%_menu.json"
     ECHO. >> %LOGFILE%
     ECHO Warning: using menuinst v1 shortcuts. >> %LOGFILE%
     ECHO menuinst v1 is marked as legacy and is no longer maintained. >> %LOGFILE%
     ECHO Please update menuinst in the base environment and reinstall %PKG_NAME%. >> %LOGFILE%
-    GOTO :exit
+GOTO :exit
 
 :exit
-    EXIT /B %ERRORLEVEL%
+    EXIT /B %ERRORLEVEL%
