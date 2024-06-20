90966b789e3f448b94ba56f956511d8ca3201fcc
Marco Esters <mesters@anaconda.com>
2024-06-20 14:32:25 -0700

    Fix syntax

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -10,8 +10,10 @@ REM prefixes with spaces in the name cannot be properly escaped.
 REM Use a temporary file as a workaround.
 SET TMPLOG="%PREFIX%\.%PKG_NAME%-post-link.tmp.log"
 "%CONDA_PYTHON_EXE%" -c "import menuinst; print(tuple(int(x) for x in menuinst.__version__.split(\".\"))[:3] < (2, 1, 0))" > %TMPLOG%
+IF ERRORLEVEL 1 EXIT 1
 SET /P OLD_MENUINST=<%TMPLOG%
-DEL %TMPLOG% IF "%OLD_MENUINST%" == "True" GOTO :use_menuinst_v1
+DEL %TMPLOG%
+IF "%OLD_MENUINST%" == "True" GOTO :use_menuinst_v1
 
 COPY /y "%PREFIX%\Menu\%PKG_NAME%_menu-v2.json.bak" "%PREFIX%\Menu\%PKG_NAME%_menu.json"
 GOTO :exit
