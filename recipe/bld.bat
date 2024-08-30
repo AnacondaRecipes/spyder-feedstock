1fcb09adfb8b11345ae8bfeec38b75ace6a93945
Marco Esters <mesters@anaconda.com>
2024-08-30 15:30:10 -0700

    Fix path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,8 +9,14 @@ REM Prepare shortcuts. menuinst v2 shortcuts should only be used startings
 REM at menuinst v2.1.1 due to bugs. The post-link script
 REM will handle which shortcut to use. One file needs to be the default
 REM menu file so that conda picks it up when running menuinst.
-IF NOT EXIST %PREFIX%\Menu mkdir %PREFIX%\Menu
-copy %RECIPE_DIR%\menu-v1.json %PREFIX%\Menu\%PKG_NAME%_menu-v1.json.bak
-copy %RECIPE_DIR%\menu-v2.json %PREFIX%\Menu\%PKG_NAME%_menu-v2.json.bak
-copy %RECIPE_DIR%\menu-v2.json %PREFIX%\Menu\%PKG_NAME%_menu.json
-copy %SRC_DIR%\img_src\spyder.ico %MENU_DIR%\
+SET MENU_DIR=%PREFIX%\Menu
+IF NOT EXIST %MENU_DIR% mkdir %MENU_DIR%
+if errorlevel 1 exit 1
+copy %RECIPE_DIR%\menu-v1.json %MENU_DIR%\%PKG_NAME%_menu-v1.json.bak
+if errorlevel 1 exit 1
+copy %RECIPE_DIR%\menu-v2.json %MENU_DIR%\%PKG_NAME%_menu-v2.json.bak
+if errorlevel 1 exit 1
+copy %RECIPE_DIR%\menu-v2.json %MENU_DIR%\%PKG_NAME%_menu.json
+if errorlevel 1 exit 1
+copy %SRC_DIR%\img_src\spyder.ico %MENU_DIR%\spyder.ico
+if errorlevel 1 exit 1
