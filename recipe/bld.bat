1ab35ade4b631f829a4c6d8fff24c28cf5e1ea3e
Marco Esters <mesters@anaconda.com>
2025-06-11 13:21:47 -0700

    Use MENU_DIR consistently

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,9 @@
-%PYTHON% -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir -vvv
+"%PYTHON%" -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir -vvv
 if errorlevel 1 exit 1
 
-del %SCRIPTS%\spyder_win_post_install.py
-del %SCRIPTS%\spyder.bat
-del %SCRIPTS%\spyder
+del "%SCRIPTS%\spyder_win_post_install.py"
+del "%SCRIPTS%\spyder.bat"
+del "%SCRIPTS%\spyder"
 
 REM Prepare shortcuts. menuinst v2 shortcuts should only be used starting
 REM at menuinst v2.1.1 due to bugs. The post-link script
@@ -12,11 +12,11 @@ REM menu file so that conda picks it up when running menuinst.
 SET "MENU_DIR=%PREFIX%\Menu"
 IF NOT EXIST "%MENU_DIR%" MKDIR "%MENU_DIR%"
 if errorlevel 1 exit 1
-copy "%RECIPE_DIR%\menu-v1.json" "%PREFIX%\Menu\%PKG_NAME%_menu-v1.json.bak"
+copy "%RECIPE_DIR%\menu-v1.json" "%MENU_DIR%\%PKG_NAME%_menu-v1.json.bak"
 if errorlevel 1 exit 1
-copy "%RECIPE_DIR%\menu-v2.json" "%PREFIX%\Menu\%PKG_NAME%_menu-v2.json.bak"
+copy "%RECIPE_DIR%\menu-v2.json" "%MENU_DIR%\%PKG_NAME%_menu-v2.json.bak"
 if errorlevel 1 exit 1
-copy "%RECIPE_DIR%\menu-v2.json" "%PREFIX%\Menu\%PKG_NAME%_menu.json"
+copy "%RECIPE_DIR%\menu-v2.json" "%MENU_DIR%\%PKG_NAME%_menu.json"
 if errorlevel 1 exit 1
-copy %SRC_DIR%\img_src\spyder.ico %MENU_DIR%\spyder.ico
+copy "%SRC_DIR%\img_src\spyder.ico" "%MENU_DIR%\spyder.ico"
 if errorlevel 1 exit 1
